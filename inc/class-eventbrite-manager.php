7b9af898db0dda971fae16d3a870287fa5721cd7
automattic <automattic@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-23 23:14:27 +0000

    Tagging version 1.0.5.
    git-svn-id: https://plugins.svn.wordpress.org/eventbrite-api/tags/1.0.5@1143944 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- inc/class-eventbrite-manager.php
+++ inc/class-eventbrite-manager.php
@@ -51,12 +51,12 @@ class Eventbrite_Manager {
 		}
 
 		// Return a cached result if we have one.
-		if ( ! $force ) {
-			$cached = $this->get_cache( $endpoint, $params );
-			if ( ! empty( $cached ) ) {
-				return $cached;
-			}
-		}
+		// if ( ! $force ) {
+		// 	$cached = $this->get_cache( $endpoint, $params );
+		// 	if ( ! empty( $cached ) ) {
+		// 		return $cached;
+		// 	}
+		// }
 
 		// Make a fresh request and cache it.
 		$request = Eventbrite_API::call( $endpoint, $params, $id );
@@ -348,7 +348,7 @@ class Eventbrite_Manager {
 		$event['post_date']     = ( isset( $api_event->start->local ) )      ? $api_event->start->local      : '';
 		$event['post_date_gmt'] = ( isset( $api_event->start->utc ) )        ? $api_event->start->utc        : '';
 		$event['url']           = ( isset( $api_event->url ) )               ? $api_event->url               : '';
-		$event['logo_url']      = ( isset( $api_event->logo_url ) )          ? $api_event->logo_url          : '';
+		$event['logo_url']      = ( isset( $api_event->logo->url ) )         ? $api_event->logo->url         : '';
 		$event['start']         = ( isset( $api_event->start ) )             ? $api_event->start             : '';
 		$event['end']           = ( isset( $api_event->end ) )               ? $api_event->end               : '';
 		$event['organizer']     = ( isset( $api_event->organizer ) )         ? $api_event->organizer         : '';
